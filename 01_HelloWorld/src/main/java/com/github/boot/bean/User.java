3f8e1a17dab68bf78097d83e72c494bc0e0cbe67
andochiwa <a1066079469@gmail.com>
2021-03-04 07:57:54 +0900

    introduction

--- 01_HelloWorld/src/main/java/com/github/boot/bean/User.java
+++ 01_HelloWorld/src/main/java/com/github/boot/bean/User.java
@@ -1,43 +1,18 @@
 package com.github.boot.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author HAN
  * @version 1.0
  * @create 03-04-4:15
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String name;
     private Integer age;
-
-    public User() {
-    }
-
-    public User(String name, Integer age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
