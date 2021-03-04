32974997fd2613cdcf9dfac8b488b7107f1a0315
andochiwa <a1066079469@gmail.com>
2021-03-05 04:32:39 +0900

    web

--- 03_web/src/main/java/com/github/boot/controller/HelloController.java
+++ 03_web/src/main/java/com/github/boot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.github.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,6 +15,27 @@ public class HelloController {
     @RequestMapping("/3.jpg")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "GET-zhangsan";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveUser(){
+        return "POST-zhangsan";
+    }
+
+
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "PUT-zhangsan";
+    }
+
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-zhangsan";
     }
 
 }
