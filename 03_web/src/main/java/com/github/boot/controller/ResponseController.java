84a40c2e6e35a17c6bb1e82e2217e09c2b308c3b
andochiwa <a1066079469@gmail.com>
2021-03-06 01:30:30 +0900

    web

--- 03_web/src/main/java/com/github/boot/controller/ResponseController.java
+++ 03_web/src/main/java/com/github/boot/controller/ResponseController.java
@@ -15,6 +15,16 @@ import java.util.Date;
 @Controller
 public class ResponseController {
 
+    /**
+     * 1.浏览器发请求直接返回xml时 [application/xml]  JacksonXmlConverter处理
+     * 2.如果是ajax请求，返回json [application/json] JacksonJsonConverter处理
+     * 3.如果返回自定义协议数据    [application/xxx]  自定义xxxConverter 处理
+     *
+     * 步骤：
+     * 1.添加自定义的MessageConverter进系统低层
+     * 2.系统低层会统计出所有MessageConverter能操作哪些类型
+     * 3.客户端内容协商[xxx -> xxx]
+     */
     @ResponseBody
     @GetMapping("/test/person")
     public Person person() {
---
+++ 03_web/src/main/java/com/github/boot/converter/MyConverter.java
@@ -0,0 +1,55 @@
+package com.github.boot.converter;
+
+import com.github.boot.bean.Person;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * 自定义的Converter
+ * @author HAN
+ * @version 1.0
+ * @create 03-06-1:21
+ */
+public class MyConverter implements HttpMessageConverter<Person> {
+    @Override
+    public boolean canRead(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    /**
+     * 服务器要统计所以MessageConverter都能写出哪些内容类型
+     *
+     * application/x-me
+     */
+    @Override
+    public List<MediaType> getSupportedMediaTypes() {
+        return MediaType.parseMediaTypes("application/x-me");
+    }
+
+    @Override
+    public Person read(Class<? extends Person> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+        return null;
+    }
+
+    @Override
+    public void write(Person person, MediaType contentType, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+        // 自定义协议数据的写出
+        String data = person.getUserName() + ";" + person.getAge() + ";" + person.getBirth();
+        // 把数据写到流里去
+        OutputStream body = outputMessage.getBody();
+        body.write(data.getBytes());
+    }
+}
