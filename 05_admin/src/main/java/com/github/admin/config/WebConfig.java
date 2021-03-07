e5a4264c2dab873cdc2d980c1dd47fa48320221c
andochiwa <a1066079469@gmail.com>
2021-03-08 06:53:23 +0900

    Interceptor

--- 05_admin/src/main/java/com/github/admin/config/WebConfig.java
+++ 05_admin/src/main/java/com/github/admin/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.github.admin.config;
 
-import com.github.interceptor.LoginInterceptor;
+import com.github.admin.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
