494a9ac038ec6049e50d846cc98227e5b8280ce5
andochiwa <a1066079469@gmail.com>
2021-03-09 01:20:43 +0900

    DB

--- 06_data/src/test/java/com/github/boot/ApplicationTests.java
+++ 06_data/src/test/java/com/github/boot/ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.github.boot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
+@Slf4j
 class ApplicationTests {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    DataSource dataSource;
 
     @Test
     void contextLoads() {
+        String sql = "select * from book";
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+        maps.forEach(data -> data.forEach((k, v) -> {
+            System.out.println(k + ": " + v);
+        }));
+        log.info("dbPool type: {}", dataSource.getClass());
     }
-
 }
