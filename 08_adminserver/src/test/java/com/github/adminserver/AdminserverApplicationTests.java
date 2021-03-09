677ac76e197929eaeb77463971fff97e1668266b
andochiwa <a1066079469@gmail.com>
2021-03-10 04:38:41 +0900

    actuator-adminServer

---
+++ 08_adminserver/src/test/java/com/github/adminserver/AdminserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
