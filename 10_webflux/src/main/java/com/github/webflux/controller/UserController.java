c25b619f82e59a837a7c71d8b8db1e46017de65d
andochiwa <a1066079469@gmail.com>
2021-08-26 23:18:12 +0900

    comparison of webflux and spring mvc

--- 10_webflux/src/main/java/com/github/webflux/controller/UserController.java
+++ 10_webflux/src/main/java/com/github/webflux/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.github.webflux.controller;
 
 import com.github.webflux.entity.User;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author HAN
@@ -11,11 +15,32 @@ import reactor.core.publisher.Mono;
  * @since 08-26-19:22
  */
 @RestController
+@Slf4j
 public class UserController {
 
+    // webflux
     @GetMapping("/mono")
     public Mono<User> getUserToMono() {
-        return Mono.just(new User("hello", "hello@gmail.com"));
+        log.info("webflux get start");
+        Mono<User> userMono = Mono.fromSupplier(this::getUser);
+        log.info("webflux get end");
+        return userMono;
+
+    }
+
+    // spring mvc
+    @GetMapping("/hello")
+    public User getUserByMvc() {
+        log.info("spring mvc get start");
+        User user = getUser();
+        log.info("spring mvc get end");
+        return user;
+    }
+
+    @SneakyThrows
+    private User getUser() {
+        TimeUnit.SECONDS.sleep(5);
+        return new User("hello", "hello@gmail.com");
     }
 
 }
